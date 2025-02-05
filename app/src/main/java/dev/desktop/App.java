56e700c34fc89a51213386fea5df652f591eb949
Bruce Li <isanoob1897@outlook.com>
2025-02-04 21:14:55 -0800

    added error handling

--- app/src/main/java/dev/desktop/App.java
+++ app/src/main/java/dev/desktop/App.java
@@ -1,1 +1,16 @@
 package dev.desktop;
+// import file processing packages and scanner
+import java.util.Scanner;
+import java.io.File;
+
+// the main app class for executing code.
+public class App {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("Error!:No input files!");
+            System.exit(64);
+        }
+        File file = new File(args[0]);
+        // read logic
+    }
+}

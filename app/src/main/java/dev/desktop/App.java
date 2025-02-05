3a56981b9d4ee80972697dca69eb940e7f8f6805
Bruce Li <isanoob1897@outlook.com>
2025-02-05 15:59:24 +0000

    add read logic

--- app/src/main/java/dev/desktop/App.java
+++ app/src/main/java/dev/desktop/App.java
@@ -2,16 +2,27 @@ package dev.desktop;
 // import file processing packages and scanner
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 // the main app class for executing code.
 public class App {
+    public static String content;
     public static void main(String[] args) {
         if (args.length < 1) {
             System.err.println("Error!:No input files!");
             System.exit(64);
         }
-        File file = new File(args[0]);
-        System.out.println(args[0]);
+        
+        try {
+            File file = new File(args[0]);
+            Scanner reader = new Scanner(file);
+            while (reader.hasNextLine()) {
+                content += reader.nextLine();
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Error!:File not Found!");
+            System.exit(126);
+        }
         // read logic
     }
 }

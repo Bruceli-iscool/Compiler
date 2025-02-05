aed5040570a9ffba4702d79153561909e918b6e4
Bruce Li <isanoob1897@outlook.com>
2025-02-05 18:56:37 +0000

    start on lexer
    Signed-off-by: Bruce Li <isanoob1897@outlook.com>

---
+++ app/src/main/java/dev/desktop/Lexer.java
@@ -0,0 +1,20 @@
+package dev.desktop;
+
+/**
+ * Lexer class
+ */
+public class Lexer {
+    // the content to lex
+    private String content;
+    Lexer(String input) {
+        // set the content
+        content = input;
+    } 
+    /**
+     * A method to return a list of tokens
+     */
+    protected void lex() {
+        // todo
+    }
+}
+
